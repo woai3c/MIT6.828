64ebb8bd9184d0653bcd87d2101500802e01d554
woai3c <411020382@qq.com>
2020-03-25 22:48:20 +0800

    feat: 添加 Lab5 练习 7 代码

--- kern/syscall.c
+++ kern/syscall.c
@@ -134,7 +134,14 @@ sys_env_set_trapframe(envid_t envid, struct Trapframe *tf)
 	// LAB 5: Your code here.
 	// Remember to check whether the user has supplied us with a good
 	// address!
-	panic("sys_env_set_trapframe not implemented");
+	struct Env *env;
+	int r;
+	if ((r = envid2env(envid, &env, 1)) < 0) return r;
+	tf->tf_eflags |= FL_IF;
+	tf->tf_eflags &= ~FL_IOPL_MASK;
+	tf->tf_cs = GD_UT | 3;
+	env->env_tf = *tf;
+	return 0;
 }
 
 // Set the page fault upcall for 'envid' by modifying the corresponding struct
@@ -407,6 +414,8 @@ syscall(uint32_t syscallno, uint32_t a1, uint32_t a2, uint32_t a3, uint32_t a4, 
 		case SYS_ipc_recv:
 			return sys_ipc_recv((void *) a1);
 			break;
+		case SYS_env_set_trapframe:
+			return sys_env_set_trapframe(a1, (struct Trapframe *) a2);
 		default:
 			return -E_INVAL;
 	}
